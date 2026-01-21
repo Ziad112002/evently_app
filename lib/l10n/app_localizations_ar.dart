94e23cac8843ea0e2fa9a990d7d270dd6cb35fcd
ziad <mqpz596@gmail.com>
2026-01-21 15:49:48 +0200

    app localization

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,125 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get loginTitle => 'تسجيل الدخول إلى حسابك';
+
+  @override
+  String get emailHint => 'أدخل بريدك الإلكتروني';
+
+  @override
+  String get passwordHint => 'أدخل كلمة المرور';
+
+  @override
+  String get forgotPassword => 'نسيت كلمة المرور؟';
+
+  @override
+  String get loginButton => 'تسجيل الدخول';
+
+  @override
+  String get noAccount => 'ليس لديك حساب؟';
+
+  @override
+  String get signup => 'إنشاء حساب';
+
+  @override
+  String get orText => 'أو';
+
+  @override
+  String get googleLogin => 'تسجيل الدخول باستخدام جوجل';
+
+  @override
+  String get errorTitle => 'خطأ!';
+
+  @override
+  String get errorMessage => 'يرجى المحاولة مرة أخرى لاحقًا';
+
+  @override
+  String get dialogCancel => 'إلغاء';
+
+  @override
+  String get dialogOk => 'موافق';
+
+  @override
+  String get createYourAccount => 'إنشاء حسابك';
+
+  @override
+  String get enterYourName => 'أدخل اسمك';
+
+  @override
+  String get confirmYourPassword => 'أكد كلمة المرور';
+
+  @override
+  String get signUpWithGoogle => 'التسجيل بواسطة جوجل';
+
+  @override
+  String get alreadyHaveAccount => 'لديك حساب بالفعل؟ ';
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get forgetPassword => 'نسيت كلمة المرور';
+
+  @override
+  String get resetPassword => 'إعادة تعيين كلمة المرور';
+
+  @override
+  String get onboardingFindTitle => 'اكتشف فعاليات تلهمك';
+
+  @override
+  String get onboardingFindDesc =>
+      'انغمس في عالم من الفعاليات المصممة خصيصًا لتناسب اهتماماتك الفريدة. سواء كنت مهتمًا بالموسيقى الحية، ورش العمل الفنية، التواصل المهني، أو مجرد اكتشاف تجارب جديدة، لدينا شيء للجميع. توصياتنا المنتقاة بعناية ستساعدك على الاستكشاف، التواصل، والاستفادة القصوى من كل فرصة حولك.';
+
+  @override
+  String get onboardingManageTitle => 'تخطيط الفعاليات بسهولة تامة';
+
+  @override
+  String get onboardingManageDesc =>
+      'ودّع تعقيدات تنظيم الفعاليات مع أدوات التخطيط الشاملة لدينا. من إعداد الدعوات وإدارة الردود، إلى جدولة التذكيرات وتنسيق التفاصيل، نحن نغطي كل شيء. خطط بسهولة وركز على ما يهم حقًا – خلق تجربة لا تُنسى لك ولضيوفك.';
+
+  @override
+  String get onboardingConnectTitle => 'تواصل مع أصدقائك وشارك اللحظات';
+
+  @override
+  String get onboardingConnectDesc =>
+      'اجعل كل فعالية لا تُنسى من خلال مشاركة التجربة مع الآخرين. منصتنا تتيح لك دعوة الأصدقاء، إبقاء الجميع على اطلاع، والاحتفال باللحظات معًا. التقط وشارك الحماس مع شبكتك، لتعيد عيش أجمل الذكريات وتحتفظ بها.';
+
+  @override
+  String get personalizeYourExperience => 'خصص تجربتك';
+
+  @override
+  String get choosePreferredThemeAndLanguage =>
+      'اختر الثيم واللغة المفضلة لديك لبدء تجربة مريحة ومخصصة تناسب أسلوبك.';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get english => 'الإنجليزية';
+
+  @override
+  String get arabic => 'العربية';
+
+  @override
+  String get theme => 'الثيم';
+
+  @override
+  String get letsStart => 'هيا نبدأ';
+
+  @override
+  String get skip => 'تخطي';
+
+  @override
+  String get next => 'التالي';
+
+  @override
+  String get getStarted => 'هيا نبدأ';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,125 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get loginTitle => 'Login to your account';
+
+  @override
+  String get emailHint => 'Enter your email';
+
+  @override
+  String get passwordHint => 'Enter your password';
+
+  @override
+  String get forgotPassword => 'Forgot Password?';
+
+  @override
+  String get loginButton => 'Login';
+
+  @override
+  String get noAccount => 'Don’t have an account?';
+
+  @override
+  String get signup => 'Signup';
+
+  @override
+  String get orText => 'Or';
+
+  @override
+  String get googleLogin => 'Login with Google';
+
+  @override
+  String get errorTitle => 'Error!';
+
+  @override
+  String get errorMessage => 'Please! try again later';
+
+  @override
+  String get dialogCancel => 'Cancel';
+
+  @override
+  String get dialogOk => 'OK';
+
+  @override
+  String get createYourAccount => 'Create your account';
+
+  @override
+  String get enterYourName => 'Enter your name';
+
+  @override
+  String get confirmYourPassword => 'Confirm your password';
+
+  @override
+  String get signUpWithGoogle => 'Sign up with Google';
+
+  @override
+  String get alreadyHaveAccount => 'Already have an account? ';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get forgetPassword => 'Forget Password';
+
+  @override
+  String get resetPassword => 'Reset password';
+
+  @override
+  String get onboardingFindTitle => 'Find Events That Inspire You';
+
+  @override
+  String get onboardingFindDesc =>
+      'Dive into a world of events crafted to fit your unique interests. Whether you\'re into live music, art workshops, professional networking, or simply discovering new experiences, we have something for everyone. Our curated recommendations will help you explore, connect, and make the most of every opportunity around you.';
+
+  @override
+  String get onboardingManageTitle => 'Effortless Event Planning';
+
+  @override
+  String get onboardingManageDesc =>
+      'Take the hassle out of organizing events with our all-in-one planning tools. From setting up invites and managing RSVPs to scheduling reminders and coordinating details, we’ve got you covered. Plan with ease and focus on what matters – creating an unforgettable experience for you and your guests.';
+
+  @override
+  String get onboardingConnectTitle => 'Connect with Friends & Share Moments';
+
+  @override
+  String get onboardingConnectDesc =>
+      'Make every event memorable by sharing the experience with others. Our platform lets you invite friends, keep everyone in the loop, and celebrate moments together. Capture and share the excitement with your network, so you can relive the highlights and cherish the memories.';
+
+  @override
+  String get personalizeYourExperience => 'Personalize Your Experience';
+
+  @override
+  String get choosePreferredThemeAndLanguage =>
+      'Choose your preferred theme and language to get started with a comfortable, tailored experience that suits your style.';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get arabic => 'Arabic';
+
+  @override
+  String get theme => 'Theme';
+
+  @override
+  String get letsStart => 'Let\'s start';
+
+  @override
+  String get skip => 'Skip';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get getStarted => 'Get started';
+}
